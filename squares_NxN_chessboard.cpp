c7185e720f127afb05a70a2f65d9aa986675542b
Aniket <70195348+AniketKharpatil@users.noreply.github.com>
2022-01-04 20:54:37 +0530

    Create squares_NxN_chessboard.cpp

---
+++ squares_NxN_chessboard.cpp
@@ -0,0 +1,34 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long N) {
+        // code here
+         long res =0;
+       while(N>=1){
+           res = res + (N*N);
+           N--;
+       }
+      return res; 
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
